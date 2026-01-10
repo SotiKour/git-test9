95980ece91873699f18cfa081c2e0396f8320b1e
sotikour <sotikour@hotmail.com>
2026-01-10 12:01:52 +0200

    feat: add MulApp

---
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.testgit;
+
+public class MulApp {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static int miltiply(int a, int b){
+        return a*b;
+    }
+
+}
